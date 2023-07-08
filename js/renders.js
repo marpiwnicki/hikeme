6261b698c811216e494f8e621f38ffc945118c82
Marek Piwnicki <marpiwnicki@gmail.com>
2023-07-08 13:19:14 +0200

    Adding renters.html page

---
+++ js/renders.js
@@ -0,0 +1,24 @@
+let activeImageContainer = document.querySelector(".active-render");
+let allImages = document.querySelectorAll(".render-image-container");
+
+let activeRenderNumber = 0;
+
+window.addEventListener("wheel", handleScroll);
+
+function handleScroll(e) {
+    console.log(activeRenderNumber);
+    if (e.deltaY < 0) {
+        if (activeRenderNumber > 0) {
+            allImages[activeRenderNumber].classList.remove("active-render");
+            activeRenderNumber = activeRenderNumber - 1;
+            allImages[activeRenderNumber].classList.add("active-render");
+        }
+    }
+    if (e.deltaY > 0) {
+        if (activeRenderNumber < allImages.length - 1) {
+            allImages[activeRenderNumber].classList.remove("active-render");
+            activeRenderNumber = activeRenderNumber + 1;
+            allImages[activeRenderNumber].classList.add("active-render");
+        }
+    }
+}

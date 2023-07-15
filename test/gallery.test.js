0bb883264c7009c8d0b36c5556756844f5cdc551
Marek Piwnicki <marpiwnicki@gmail.com>
2023-07-15 16:04:30 +0200

    adding simple tests in puppeteer (along with node)

---
+++ test/gallery.test.js
@@ -0,0 +1,64 @@
+const puppeteer = require('puppeteer');
+const assert = require('assert');
+
+const localURL = 'http://127.0.0.1:5500/';
+
+
+describe('Main Gallery tests', () => {
+    let browser;
+    let page;
+
+    before(async () => {
+        // Launch a new browser instance
+        browser = await puppeteer.launch();
+    });
+
+    after(async () => {
+        // Close the browser once all tests are completed
+        await browser.close();
+    });
+
+    beforeEach(async () => {
+        // Create a new page instance before each test
+        page = await browser.newPage();
+    });
+
+    afterEach(async function () {
+        if (this.currentTest.state === 'failed') {
+            const screenshotPath = `test/failed tests - screenshots/TEST FAILURE - ${this.currentTest.fullTitle()}.png`;
+            console.log('saving screenshot in ' + screenshotPath);
+            await page.screenshot({ path: screenshotPath });
+        }
+        await page.close();
+    });
+
+    it('gallery.html title', async () => {
+        await page.goto(localURL + 'gallery.html');
+        const title = await page.title();
+        assert.strictEqual(title, 'Gallery');
+    });
+
+    it('menu is visible', async () => {
+        await page.goto(localURL + 'gallery.html');
+        await page.waitForSelector("#menu");
+    });
+
+    it('category title is available', async () => {
+        await page.goto(localURL + 'gallery.html');
+        await page.waitForSelector(".category-title");
+
+        const categoryTitles = await page.evaluate(() => {
+            const categoryTitlesDivs = document.querySelectorAll('.category-title'); // Replace 'div' with the appropriate selector for your <div> element
+            const categoryTitles = [];
+            categoryTitlesDivs.forEach(element => {
+                categoryTitles.push(element.innerText);
+            });
+            return categoryTitles;
+        });
+        assert.strictEqual(categoryTitles.length, 25);
+
+    });
+
+
+
+});
---
+++ test/sanityPageLoad.test.js
@@ -0,0 +1,134 @@
+const puppeteer = require('puppeteer');
+const assert = require('assert');
+
+const localURL = 'http://127.0.0.1:5500/';
+
+describe('HikeMe sanity Test Suite - page loading', () => {
+    let browser;
+    let page;
+
+    before(async () => {
+        // Launch a new browser instance
+        browser = await puppeteer.launch();
+    });
+
+    after(async () => {
+        // Close the browser once all tests are completed
+        await browser.close();
+    });
+
+    beforeEach(async () => {
+        // Create a new page instance before each test
+        page = await browser.newPage();
+    });
+
+    afterEach(async function () {
+        if (this.currentTest.state === 'failed') {
+            const screenshotPath = `test/failed tests - screenshots/TEST FAILURE - ${this.currentTest.fullTitle()}.png`;
+            console.log('saving screenshot in ' + screenshotPath);
+            await page.screenshot({ path: screenshotPath });
+        }
+        await page.close();
+    });
+
+    it('index.html', async () => {
+        await page.goto(localURL + 'index.html');
+        const title = await page.title();
+        assert.strictEqual(title, 'HikeMe');
+    });
+    it('gallery.html', async () => {
+        await page.goto(localURL + 'gallery.html');
+        const title = await page.title();
+        assert.strictEqual(title, 'Gallery');
+    });
+    it('about.html', async () => {
+        await page.goto(localURL + 'about.html');
+        const title = await page.title();
+        assert.strictEqual(title, 'About');
+    });
+    it('gallery - landscapes.html', async () => {
+        await page.goto(localURL + 'html/gallery/landscapes.html');
+        const title = await page.title();
+        assert.strictEqual(title, 'Landscapes');
+    });
+
+    it('gallery - landscapes-morning.html', async () => {
+        await page.goto(localURL + 'html/gallery/landscapes-morning.html');
+        const title = await page.title();
+        assert.strictEqual(title, 'Landscapes morning');
+    });
+
+    it('gallery - landscapes-colors-dusk.html', async () => {
+        await page.goto(localURL + 'html/gallery/landscapes-colors-dusk.html');
+        const title = await page.title();
+        assert.strictEqual(title, 'Colors of the dusk');
+    });
+
+    it('gallery - landscapesp-pink-rocks.html', async () => {
+        await page.goto(localURL + 'html/gallery/landscapes-pink-rocks.html');
+        const title = await page.title();
+        assert.strictEqual(title, 'Pink rocks');
+    });
+
+    it('gallery - landscapesp-cold.html', async () => {
+        await page.goto(localURL + 'html/gallery/landscapes-cold.html');
+        const title = await page.title();
+        assert.strictEqual(title, 'Cold landscapes');
+    });
+
+    it('gallery - night.html', async () => {
+        await page.goto(localURL + 'html/gallery/night.html');
+        const title = await page.title();
+        assert.strictEqual(title, 'Night');
+    });
+    it('gallery - bikepacking.html', async () => {
+        await page.goto(localURL + 'html/gallery/bikepacking.html');
+        const title = await page.title();
+        assert.strictEqual(title, 'Bikepacking');
+    });
+    it('gallery - nature.html', async () => {
+        await page.goto(localURL + 'html/gallery/nature.html');
+        const title = await page.title();
+        assert.strictEqual(title, 'Nature');
+    });
+    it('gallery - herbarium.html', async () => {
+        await page.goto(localURL + 'html/gallery/herbarium.html');
+        const title = await page.title();
+        assert.strictEqual(title, 'Herbarium');
+    });
+    it('gallery - fungarium.html', async () => {
+        await page.goto(localURL + 'html/gallery/fungarium.html');
+        const title = await page.title();
+        assert.strictEqual(title, 'Fungarium');
+    });
+    it('gallery - animals.html', async () => {
+        await page.goto(localURL + 'html/gallery/animals.html');
+        const title = await page.title();
+        assert.strictEqual(title, 'Animals');
+    });
+    it('gallery - light.html', async () => {
+        await page.goto(localURL + 'html/gallery/light.html');
+        const title = await page.title();
+        assert.strictEqual(title, 'Light');
+    });
+    it('gallery - smoke.html', async () => {
+        await page.goto(localURL + 'html/gallery/smoke.html');
+        const title = await page.title();
+        assert.strictEqual(title, 'Smoke');
+    });
+    it('gallery - renders.html', async () => {
+        await page.goto(localURL + 'html/gallery/renders.html');
+        const title = await page.title();
+        assert.strictEqual(title, 'Renders');
+    });
+    // it('gallery - experimental.html', async () => {
+    //     await page.goto(localURL + 'html/gallery/experimental.html');
+    //     const title = await page.title();
+    //     assert.strictEqual(title, 'Experimental');
+    // });
+    // it('gallery - industrial.html', async () => {
+    //     await page.goto(localURL + 'html/gallery/industrial.html');
+    //     const title = await page.title();
+    //     assert.strictEqual(title, 'Industrial');
+    // });
+});

a142e338829d4d933ef1df7c5b1266c11452e6f1
Marek Piwnicki <marpiwnicki@gmail.com>
2023-07-17 23:01:28 +0200

    added second night location and switch handling

--- js/nigth.js
+++ js/nigth.js
@@ -1,7 +1,20 @@
 let mainImageLayerPerson = document.querySelector(".night-image-main-layer-person");
 let mainImageLayerMountain = document.querySelector(".night-image-main-layer-mountain");
 let mainImageLayerSky = document.querySelector(".night-image-main-layer-sky");
+
+
+
+let allStarsImagesContainers = document.querySelectorAll(".image-container-stars");
 let allStarsImages = document.querySelectorAll(".night-image-stars");
+let allStarsImages2 = document.querySelectorAll(".night-image-stars2");
+let switchButton = document.querySelector(".switch-button");
+let locationButtons = document.querySelectorAll(".location-button");
+
+
+
+let activeStars = 0;
+locationButtons[activeStars].classList.add("location-button-selected");
+
 
 let scrollPosition = 0;
 let activeStarNumber = 0;
@@ -15,6 +28,17 @@ let timerId;
 
 window.addEventListener("wheel", handleScroll);
 window.addEventListener("mousemove", handleMouseMove);
+switchButton.addEventListener("click", handleSwitchButton);
+
+function handleSwitchButton(e) {
+
+    locationButtons[activeStars].classList.remove("location-button-selected");
+    if (activeStars == 0) activeStars = 1; else activeStars = 0;
+    allStarsImagesContainers.forEach(container => { container.classList.remove("inactive"); });
+    allStarsImagesContainers[activeStars].classList.add("inactive");
+    locationButtons[activeStars].classList.add("location-button-selected");
+    console.log(activeStars);
+}
 
 function handleScroll(e) {
     // UP
@@ -51,6 +75,11 @@ function handleScroll(e) {
         mainImageLayerPerson.style.opacity = 0;
         mainImageLayerMountain.style.opacity = 0;
         mainImageLayerSky.style.opacity = 0;
+
+        switchButton.style.visibility = "visible";
+        locationButtons.forEach(button => { button.style.visibility = "visible" });
+
+
     }
     if (scrollPosition <= 20) {
         mainImageLayerPerson.style.marginTop = 57 + "vh";
@@ -59,20 +88,39 @@ function handleScroll(e) {
         mainImageLayerPerson.style.opacity = 1;
         mainImageLayerMountain.style.opacity = 1;
         mainImageLayerSky.style.opacity = 1;
+
+
+        switchButton.style.visibility = "collapse";
+        locationButtons.forEach(button => { button.style.visibility = "collapse" });
+
     }
 }
 
 function handleMouseMove(e) {
 
     clearTimeout(timerId);
-    activeStarNumber = Math.round((e.clientX / document.documentElement.clientWidth) * allStarsImages.length) + 1;
-    if (activeStarNumber <= allStarsImages.length - 1) {
-        allStarsImages.forEach(starImage => {
-            starImage.classList.remove("active-stars");
-        });
-        allStarsImages[activeStarNumber].classList.add("active-stars");
+    if (activeStars == 1) {
+        activeStarNumber = Math.round((e.clientX / document.documentElement.clientWidth) * allStarsImages.length) + 1;
+        if (activeStarNumber <= allStarsImages.length - 1) {
+            allStarsImages.forEach(starImage => {
+                starImage.classList.remove("active-stars");
+            });
+            allStarsImages[activeStarNumber].classList.add("active-stars");
+        }
+        timerId = setTimeout(() => {
+            allStarsImages[0].classList.add("active-stars");
+        }, 1000);
     }
-    timerId = setTimeout(() => {
-        allStarsImages[0].classList.add("active-stars");
-    }, 1000);
+    if (activeStars == 0) {
+        activeStarNumber = Math.round((e.clientX / document.documentElement.clientWidth) * allStarsImages2.length) + 1;
+        if (activeStarNumber <= allStarsImages2.length - 1) {
+            allStarsImages2.forEach(starImage => {
+                starImage.classList.remove("active-stars");
+            });
+            allStarsImages2[activeStarNumber].classList.add("active-stars");
+        }
+        timerId = setTimeout(() => {
+            allStarsImages2[0].classList.add("active-stars");
+        }, 1000);
+    }
 }

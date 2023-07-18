9398ed566291a0964b773857506fcb44820eaff2
Marek Piwnicki <marpiwnicki@gmail.com>
2023-07-18 15:29:08 +0200

    update on animals.html page

--- js/animals.js
+++ js/animals.js
@@ -1,7 +1,8 @@
-let video = document.querySelector(".video-container");
+let background = document.querySelector(".background-image");
 let documentWidth = document.documentElement.clientWidth;
 
 window.addEventListener("mousemove", handleMouseMove);
 
 function handleMouseMove(e) {
+    background.style.scale = 1.1 + e.clientX / 100000 - e.clientY / 100000;
 }
